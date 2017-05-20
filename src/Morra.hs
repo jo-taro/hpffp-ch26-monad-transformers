998d7f38dfa140f8b71f30d9461847a48ca960c1
jo-taro <jo-taro@gmail.com>
2017-05-20 14:46:39 +0900

    add parity choosing function

--- src/Morra.hs
+++ src/Morra.hs
@@ -2,5 +2,20 @@
 
 module Main where
 
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Class
+import System.Random
+
+data Parity = Even | Odd deriving (Eq,Show)
+
+chooseParity :: IO Parity
+chooseParity = do
+  choose <- randomRIO (0,1)
+  return $ case choose::Int of
+    0 -> Even
+    1 -> Odd
+
 main :: IO ()
-main = print "Morra"
+main = do
+  parity <- chooseParity
+  print $ show parity
