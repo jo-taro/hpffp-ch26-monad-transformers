0d59fc05a15a4e9d8cbccb79df96e0df1af6b88b
jo-taro <jo-taro@gmail.com>
2017-05-20 15:33:05 +0900

    add  singleRoundMorra

--- src/Morra.hs
+++ src/Morra.hs
@@ -2,11 +2,24 @@
 
 module Main where
 
+import Control.Monad.Trans.Reader
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Class
+import Control.Monad
 import System.Random
 
+
+data Config = Config { computer :: Parity }
+
 data Parity = Even | Odd deriving (Eq,Show)
+
+data Player = Computer | Human deriving (Eq,Show)
+
+data RoundResult = RoundResult
+                    { copmuter :: Int
+                    , human    :: Int
+                    , winner   :: Player
+                    } deriving (Show)
 
 chooseParity :: IO Parity
 chooseParity = do
@@ -18,6 +31,19 @@ chooseParity = do
 choosePositiveNumber :: IO Int
 choosePositiveNumber = do
   randomRIO (0, maxBound)
+
+singleRoundMorra :: ReaderT Config IO RoundResult
+singleRoundMorra = do
+  computer'     <- asks computer
+  computerNumer <- lift choosePositiveNumber
+  humanNumer    <- lift $ getLine >>= return . read
+  let winner' = if determinParity computerNumer humanNumer == computer'
+                  then Computer
+                  else Human
+  return $ RoundResult computerNumer humanNumer winner'
+
+determinParity :: Int -> Int -> Parity
+determinParity x y = if (x + y) `mod` 2 == 0 then Even else Odd
 
 main :: IO ()
 main = do
